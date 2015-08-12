e5a100d7115472200e6905099a7f57d118a229da
jmadden85 <joe@beardbro.com>
2015-08-12 02:10:57 -0400

    service

--- charmApp.js
+++ charmApp.js
@@ -3,44 +3,9 @@ angular.module('Charm', [
   'ngResource'
 ])
 .controller('MainCtrl', function ($scope, NavService) {
-  $scope.navLinks = [
-    {id: 0, name: 'About', url: '/about'},
-    {id: 1, name: 'Rooms', url: '/rooms'},
-    {id: 2, name: 'Meetings & Events', url: '/meetings'},
-    {id: 3, name: 'Neighborhood', url: '/neighborhood'},
-    {id: 3, name: 'Blog', url: '/blog'},
-    {id: 3, name: 'Careers', url: '/careers'}
-  ];
-  $scope.navStuff = NavService.get();
+  $scope.navLinks = NavService.query();
   $scope.hello = 'world';
 })
-.factory('NavService', function ($resource) {
-  var nav = $resource('/starter/server/navigation.json');
-
-  return nav;
+.factory('NavService', function($resource) {
+  return $resource('/blar/server/navigation.json');
 });
-// .factory('NavService', function ($resource, $q) {
-//   return {
-
-//     service: function () {
-//       return $resource('/starter/server/navigation.json',
-//         {},
-//         {
-//           get: {
-//             method : 'GET',
-//             isArray: false
-//           }
-//         });
-//     },
-//     get: function () {
-//       var deffered = $q.defer();
-
-//       console.log('called');
-//       this.service().get(function (response) {
-//         console.log(response);
-//         deffered.resolve(response);
-//       });
-//       return deffered.promise;
-//     }
-//   };
-// });
